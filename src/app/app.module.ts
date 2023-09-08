aa8f9bcb98825973eb354334e5ffec2a9abe5c10
Henry G Shoko <gshokx91@gmail.com>
2023-09-08 22:14:57 +0200

    created other components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { BannerComponent } from './banner/banner.component';
+import { ServiceCategoriesComponent } from './service-categories/service-categories.component';
+import { CaseStudiesComponent } from './case-studies/case-studies.component';
+import { BrandsComponent } from './brands/brands.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent, BannerComponent, ServiceCategoriesComponent, CaseStudiesComponent, BrandsComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
